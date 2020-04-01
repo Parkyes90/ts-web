10837d054b512c58e4778bded7413745e0f48087
parkyes90 <parkyes90@gmail.com>
2020-04-01 16:26:00 +0900

    feat(ProjectFileDir): Mod Collection.ts

---
+++ src/models/Collection.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosResponse } from "axios";
+import { Eventing } from "./Eventing";
+
+export class Collection<T, K> {
+  models: T[] = [];
+  events: Eventing = new Eventing();
+
+  constructor(public rootUrl: string, public deserializer: (json: K) => T) {}
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  fetch(): void {
+    axios.get(this.rootUrl).then((response: AxiosResponse<K[]>) => {
+      response.data.forEach((value) => {
+        this.models.push(this.deserializer(value));
+      });
+      this.trigger("change");
+    });
+  }
+}
