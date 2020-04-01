9354d937f1d9c8cfd81a1c2d03e3a48a3212abd6
parkyes90 <parkyes90@gmail.com>
2020-04-01 14:50:53 +0900

    feat(src/models): Add Delegations

--- src/models/Eventing.ts
+++ src/models/Eventing.ts
@@ -1,19 +1,19 @@
-type Callback = () => void;
+export type Callback = () => void;
 
 export class Eventing {
   events: { [key: string]: Callback[] } = {};
 
-  on(eventName: string, callback: Callback) {
+  on = (eventName: string, callback: Callback) => {
     const handler = this.events[eventName] || [];
     handler.push(callback);
     this.events[eventName] = handler;
-  }
-  trigger(eventName: string): void {
+  };
+  trigger = (eventName: string): void => {
     const handlers = this.events[eventName];
     if (handlers && handlers.length > 0) {
       handlers.forEach((callback) => {
         callback();
       });
     }
-  }
+  };
 }
